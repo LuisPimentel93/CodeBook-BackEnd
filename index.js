b3445d1b6530cdb92cf9bfa833a6e56d2e4db44f
Eric Bunn <Sarutke93@gmail.com>
2023-07-09 12:36:06 -0400

    added basic connection and set up

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const profileRoutes = require('./routes/Profile')
+
+const app = express()
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//routes
+app.use('/profile', profileRoutes)
 
 
+const PORT = process.env.PORT 
+
+// db connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
+
+
+app.listen(PORT, console.log(`listining on port ${PORT}`))
