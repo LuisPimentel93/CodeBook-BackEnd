f9ab6ce3ee1437048531d76ae0b562a01514ae16
Luis Pimentel <luispimentel@Luiss-MBP.lan>
2023-07-11 19:25:54 -0400

    added bcrypt to the password

--- models/Profile.js
+++ models/Profile.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const profileSchema = new mongoose.Schema({
     userName: {
@@ -29,5 +30,38 @@ const profileSchema = new mongoose.Schema({
     }
    
 })
+
+profileSchema.pre("save", function (next) {
+  const user = this
+
+  if (this.isModified("password") || this.isNew) {
+    bcrypt.genSalt(10, function (saltError, salt) {
+      if (saltError) {
+        return next(saltError)
+      } else {
+        bcrypt.hash(user.password, salt, function(hashError, hash) {
+          if (hashError) {
+            return next(hashError)
+          }
+
+          user.password = hash
+          next()
+        })
+      }
+    })
+  } else {
+    return next()
+  }
+})
+
+profileSchema.methods.comparePassword = function(password, callback) {
+  bcrypt.compare(password, this.password, function(error, isMatch) {
+    if (error) {
+      return callback(error)
+    } else {
+      callback(null, isMatch)
+    }
+  })
+}
 
 module.exports = mongoose.model('Profile', profileSchema)
