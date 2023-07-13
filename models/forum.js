6bf66937f9fc19a214decdc3ad6c5f1de70fd320
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-07-13 18:48:05 -0400

    Forum modules updated

--- models/forum.js
+++ models/forum.js
@@ -4,8 +4,20 @@ const forumSchema = new mongoose.Schema({
     getsUpdates: {
       type: Boolean,
       default: false
-    }
-   
+    },
+    ProfileName: {
+      type: String,
+      default: false
+    },
+    Date: {
+      type: Number,
+      timestamps: true
+    },
+    Body: {
+      type: String,
+      default: false
+
+    } 
 })
 
 module.exports = mongoose.model('Forum', forumSchema)
